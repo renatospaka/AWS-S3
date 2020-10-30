28e114bae443265a75c0010f923b4671dbfe8ad4
Renato Spakauskas <renatospaka@gmail.com>
2020-10-30 14:07:41 -0300

    Upload +0 Tagging

--- aws-cli/bucket.sh
+++ aws-cli/bucket.sh
@@ -10,10 +10,3 @@ aws s3api create-bucket --bucket hkh-teste01-front-door --region us-east-2  --cr
 # excluir um  bucket
 # aws s3api delete-bucket --bucket <bucket-name> --region <region-acrhonimum>
 aws s3api delete-bucket --bucket hkh-teste01-front-door --region us-east-2
-
-
-# listar TODOS os objectos de bucket
-# aws s3api list-objects-v2  --bucket <bucket-name>
-aws s3api list-objects-v2 --bucket hkh-teste01-front-door 
-aws s3api list-objects-v2 --bucket hkh-teste01-front-door  \
-    --delimiter "minha-pasta-pessoal"
